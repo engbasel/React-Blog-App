9193b150133f2a028bda774b0571fd8165a12187
Basel Embaby <engbasel2025@gmail.com>
2025-09-22 11:32:37 +0300

    `Refactored Post component to accept props for dynamic content`

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -10,34 +10,33 @@
 
 import React from "react";
 import "./Post.css"; // لو هتحط CSS في ملف منفصل
-export default function Post() {
+export default function Post({postTitle , postDescription , postImage , postAuthor, postedAt, authorAvatar}) {
   return (
     <article className="post-card">
       <div className="post-image-wrap">
         <img
           className="post-image"
-          src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
+          src={postImage}
           alt="Post"
         />
       </div>
 
       <div className="post-content">
-        <h3 className="post-title">عنوان البوست هنا</h3>
+        <h3 className="post-title"> {postTitle}</h3>
         <p className="post-desc">
-          هذا وصف تجريبي للبوست. هنا تقدر تحط سطرين أو ثلاثة عشان توضح فكرة البوست
-          بشكل مختصر. الهدف إن الوصف يعطي لمحة سريعة عن المحتوى.
+          {postDescription}
         </p>
 
         <div className="post-meta">
           <div className="author">
             <img
               className="author-avatar"
-              src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
+              src={authorAvatar}
               alt="Author"
             />
             <div className="author-info">
-              <span className="author-name">اسم الكاتب</span>
-              <span className="post-date">• 12 سبتمبر 2025</span>
+              <span className="author-name">{postAuthor}</span>
+              <span className="post-date"> {postedAt}</span>
             </div>
           </div>
 
