b8a4638dfe35fcdbb241dc030b11c1094ae57841
Basel Embaby <engbasel2025@gmail.com>
2025-09-22 11:04:38 +0300

    `Refactor: Move pages to new features folders and delete unused files`

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 // الصفحات
-import Home from "./pages/HomeView.jsx";
-import Login from "./pages/Login.jsx";
-import AddPost from "./pages/AddPost.jsx";
-import About from "./pages/About.jsx";
-import Register from "./pages/Register.jsx";
-import PageNotFound from "./pages/PangenotFound.jsx";
+import Home from "./features/home/HomeView.jsx";
+import Login from "./features/auth/Login.jsx";
+import AddPost from "./features/posts/AddPost.jsx";
+import About from "./features/about/About.jsx";
+import Register from "./features/auth/Register.jsx";
+import PageNotFound from "./features/about/PangenotFound.jsx";
 
 export default function App() {
   return (
