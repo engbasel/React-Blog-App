bd126109ca34aed99b9b60fdcd73141a146beab1
Basel Embaby <engbasel2025@gmail.com>
2025-09-22 11:43:29 +0300

    `Added useEffect hook to fetch posts data and hardcoded Post components with props`

--- src/features/home/HomeView.jsx
+++ src/features/home/HomeView.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Post from '../../components/post'
+const fetchPosts = async () => {
+  // هنا بتجيب الداتا سواء من local أو API
+  const response = await fetch('http://localhost:3000/posts');
+  const data = await response.json();
+  console.log(data);  
+};
+
+
 
 export default function Home() {
+  
+useEffect(() => {
+  fetchPosts();
+}, []);
   return (
     <div>
-        <Post/>
-        <Post/>
-        <Post/>    </div>
+        <Post postTitle="Hello World" postDescription="Hello World" postImage="https://via.placeholder.com/150" postAuthor="Post Author" postedAt="Posted At" authorAvatar="https://via.placeholder.com/150" />
+        <Post postTitle="Hello World" postDescription="Hello World" postImage="https://via.placeholder.com/150" postAuthor="Post Author" postedAt="Posted At" authorAvatar="https://via.placeholder.com/150" />
+        <Post postTitle="Hello World" postDescription="Hello World" postImage="https://via.placeholder.com/150" postAuthor="Post Author" postedAt="Posted At" authorAvatar="https://via.placeholder.com/150" />    </div>
   )
 }
