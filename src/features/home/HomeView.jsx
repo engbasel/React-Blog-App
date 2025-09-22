d4135f728d4b2ac54b431ce61604c12ac61e9409
Basel Embaby <engbasel2025@gmail.com>
2025-09-22 14:13:22 +0300

    `Added import of Link and AddButton components, and added state and AddButton component to HomeView.jsx`

--- src/features/home/HomeView.jsx
+++ src/features/home/HomeView.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Post from '../../components/post'
+import {  Link } from 'react-router-dom';
+import AddButton from '../../components/addbuttom';
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
 
   const fetchPosts = async () => {
     const response = await fetch('http://localhost:3000/posts');
@@ -24,7 +27,10 @@ useEffect(() => {
          {posts.map((post) => (
          <Post postTitle={post.title} postDescription={post.description} postImage={post.image} postAuthor={post.author} postedAt={post.postedAt} authorAvatar={post.authorAvatar} /> 
          ))}
-        
+         <div>
+
+         </div>
+         <AddButton buttonTitle="Add Post" onClick={() => setOpen(false)} navigationPath="/add" />
          </div>
   )
 }
