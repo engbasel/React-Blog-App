8b3994d486d47cd9f3a4f9d9ad6288cc19165818
Basel Embaby <engbasel2025@gmail.com>
2025-09-22 14:05:20 +0300

    `Refactored login functionality to use Firebase authentication`

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -2,53 +2,40 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Auth.css";
 import InputField from "../../components/InputField";
+import { auth } from "../../../firebase/config.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleSubmit(e) {
+  function handleLogin(e) { 
     e.preventDefault();
 
-    const saved = JSON.parse(localStorage.getItem("blog_user"));
-    if (!saved) {
-      alert("No user found. Please register first.");
-      return;
-    }
-
-    if (saved.email !== email || saved.password !== password) {
-      alert("Email or password incorrect.");
-      return;
-    }
-
-    localStorage.setItem("blog_logged_in", JSON.stringify({ name: saved.name, email: saved.email }));
-    navigate("/");
+signInWithEmailAndPassword(auth,email,password)
+.then((userCredential) => {
+  const user = userCredential.user;
+  console.log(user);
+  navigate("/");
+})
+.catch((error) => {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  console.log(errorMessage);
+  alert(errorMessage);
+});
   }
 
   return (
     <div className="auth-page">
-      <form className="auth-card" onSubmit={handleSubmit}>
+      <form className="auth-card" onSubmit={handleLogin}>
         <h2>Login</h2>
-        {/* <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-         */}
+  
 
 <InputField type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
 <InputField type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        {/* <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        /> */}
         <button type="submit" className="auth-btn">Login</button>
 
         <p className="small">
