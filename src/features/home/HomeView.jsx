85d946c424141e55ba2cc985fe3123bd41050377
Basel Embaby <engbasel2025@gmail.com>
2025-09-22 11:50:18 +0300

    `feat: update db.json and HomeView.jsx with new post data and functionality`

--- src/features/home/HomeView.jsx
+++ src/features/home/HomeView.jsx
@@ -1,23 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Post from '../../components/post'
-const fetchPosts = async () => {
-  // هنا بتجيب الداتا سواء من local أو API
-  const response = await fetch('http://localhost:3000/posts');
-  const data = await response.json();
-  console.log(data);  
-};
-
-
 
 export default function Home() {
+
+  const fetchPosts = async () => {
+    // هنا بتجيب الداتا سواء من local أو API
+    const response = await fetch('http://localhost:3000/posts');
+    const data = await response.json();
+    setPosts(data);  
+  };
   
+  
+  
+  const [posts, setPosts] = useState([]);
 useEffect(() => {
-  fetchPosts();
+    fetchPosts();
 }, []);
   return (
     <div>
-        <Post postTitle="Hello World" postDescription="Hello World" postImage="https://via.placeholder.com/150" postAuthor="Post Author" postedAt="Posted At" authorAvatar="https://via.placeholder.com/150" />
-        <Post postTitle="Hello World" postDescription="Hello World" postImage="https://via.placeholder.com/150" postAuthor="Post Author" postedAt="Posted At" authorAvatar="https://via.placeholder.com/150" />
-        <Post postTitle="Hello World" postDescription="Hello World" postImage="https://via.placeholder.com/150" postAuthor="Post Author" postedAt="Posted At" authorAvatar="https://via.placeholder.com/150" />    </div>
+        
+         {posts.map((post) => (
+         <Post postTitle={post.title} postDescription={post.description} postImage={post.image} postAuthor={post.author} postedAt={post.postedAt} authorAvatar={post.authorAvatar} /> 
+         ))}
+        
+         </div>
   )
 }
