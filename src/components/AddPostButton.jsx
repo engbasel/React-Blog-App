7b641e673c6a35a2073e89ab16c69e6ebd29cd52
Basel Embaby <engbasel2025@gmail.com>
2025-09-29 18:09:18 +0300

    `feat: updated AddPostButton component with navigation and authentication logic`

--- src/components/AddPostButton.jsx
+++ src/components/AddPostButton.jsx
@@ -1,9 +1,26 @@
+
 import React from "react";
 import "./AddPostButton.css";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase/config"; // لو عندك config للفايربيز
 
-export default function AddPostButton({ onClick }) {
+export default function AddPostButton() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    const user = auth.currentUser;
+
+    if (user) {
+      // alert("✅ You are logged in, going to Add Post screen...");
+      navigate("/add"); // غير الـ path حسب عندك
+    } else {
+      alert("⚠️ You must log in first!");
+      navigate("/login"); // غير الـ path حسب عندك
+    }
+  };
+
   return (
-    <button className="addpost-btn" onClick={onClick}>
+    <button className="addpost-btn" onClick={handleClick}>
       +
     </button>
   );
